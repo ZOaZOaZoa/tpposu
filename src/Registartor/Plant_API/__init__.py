8b539daa04e300228fdbece079e264aae150b435
ZOa <vbifbk565@gmail.com>
2026-05-11 14:41:14 +0300

    Выделение цветом если вышли за границы

--- src/Registartor/Plant_API/__init__.py
+++ src/Registartor/Plant_API/__init__.py
@@ -1,5 +1,5 @@
 from .Plant import Plant
-from .Channel import Channel, ChannelParam, Preprocessing
+from .Channel import Channel, ChannelParam, Preprocessing, MeasureError
 from .Registrator import Registrator, Action, TKI_step
 
-__all__ = ['Plant', 'Channel', 'Registrator', 'ChannelParam', 'Preprocessing', 'Action', 'TKI_step']
+__all__ = ['Plant', 'Channel', 'Registrator', 'ChannelParam', 'Preprocessing', 'Action', 'TKI_step', 'MeasureError']
